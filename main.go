f5b3f38a71d696382e51841ebf5ed9f5f33ab160
Spark Huang <50857240+sparkdoaz@users.noreply.github.com>
2023-12-16 05:57:49 +0800

    add cache

--- main.go
+++ main.go
@@ -1,15 +1,21 @@
 package main
 
 import (
+	"context"
 	"database/sql"
-
+	"encoding/json"
+	"fmt"
 	_ "fmt"
 	_ "log"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/lib/pq"
+	"github.com/redis/go-redis/v9"
 )
+
+var ctx = context.Background()
 
 func main() {
 	// 初始化 Gin 路由
@@ -24,8 +30,22 @@ func main() {
 	}
 	defer db.Close()
 
+	// 创建 Redis 客户端连接
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	// 建立连接
+	str, err := client.Ping(ctx).Result()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Print(str)
+
 	// 設定查詢物流數據的端點
-	r.GET("/query", queryLogisticsHandler(db))
+	r.GET("/query", queryLogisticsHandler(db, client))
 
 	// 啟動 Web 伺服器
 	if err := r.Run("127.0.0.1:3000"); err != nil {
@@ -34,7 +54,7 @@ func main() {
 }
 
 // queryLogisticsHandler 是處理查詢物流數據的處理程序
-func queryLogisticsHandler(db *sql.DB) gin.HandlerFunc {
+func queryLogisticsHandler(db *sql.DB, redis *redis.Client) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		sno := c.DefaultQuery("sno", "")
 		if sno == "" {
@@ -49,7 +69,7 @@ func queryLogisticsHandler(db *sql.DB) gin.HandlerFunc {
 			return
 		}
 
-		details, err := getPackageDetailsInDB(sno, db)
+		details, err := get(redis, db, sno, ctx)
 
 		if err != nil {
 			if err == sql.ErrNoRows {
@@ -130,4 +150,43 @@ func getPackageDetailsInDB(sno string, db *sql.DB) (PackageDetails, error) {
 	}
 
 	return pd, nil
+}
+
+func get(client *redis.Client, db *sql.DB, sno string, ctx context.Context) (PackageDetails, error) {
+	cacheResult, err := client.HGet(ctx, "logistics_cache", sno).Result()
+
+	if err == redis.Nil {
+		// 缓存不存在，从数据库获取物流信息
+		packageDetails, _ := getPackageDetailsInDB(sno, db)
+
+		// 将物流信息存入缓存
+		if err := setLogisticsInfoInCache(client, ctx, sno, packageDetails); err != nil {
+			fmt.Println("Error setting cache:", err)
+		}
+
+		// 返回物流信息给用户
+		return packageDetails, nil
+	} else if err != nil {
+		// 出现错误
+		return PackageDetails{}, err
+	} else {
+		// 缓存命中，直接返回缓存的物流信息
+		var packageDetails PackageDetails
+		if err := json.Unmarshal([]byte(cacheResult), &packageDetails); err != nil {
+			return PackageDetails{}, err
+		}
+		return packageDetails, nil
+	}
+}
+
+// 设置物流信息到缓存
+func setLogisticsInfoInCache(client *redis.Client, ctx context.Context, sno string, packageDetails PackageDetails) error {
+	// 将物流信息转换为 JSON 格式
+	data, err := json.Marshal(packageDetails)
+	if err != nil {
+		return err
+	}
+	// 设置缓存并指定过期时间（根据业务需求设置）
+	client.HSet(ctx, "logistics_cache", sno, data).Err()
+	return client.Expire(ctx, "logistics_cache", time.Hour).Err()
 }
